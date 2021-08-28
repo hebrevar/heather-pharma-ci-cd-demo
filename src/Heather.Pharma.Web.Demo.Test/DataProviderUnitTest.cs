93c172cacd6888fd0920e879f48625139dea72d7
Heather Brevard <hebrevar@microsoft.com>
2021-08-28 13:48:37 -0500

    Initial commit of source code, test code and infra as code

---
+++ src/Heather.Pharma.Web.Demo.Test/DataProviderUnitTest.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Heather.Pharma.Web.Demo.Test
+{
+	[TestClass]
+	public class DataProviderUnitTest
+	{
+		[TestMethod]
+		public void TestMethodShouldReturnFalse()
+		{
+			// arrange
+
+			// act
+
+			// assert
+			Assert.IsFalse(false, "Demo unit test should return false.");
+		}
+	}
+}
